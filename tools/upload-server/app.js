7698867bb9eca7dbf26a6c49d87ed4bedf0b37a2
robin <robin.prillwitz@gmail.com>
2024-08-23 18:57:34 +0200

    server style change

--- tools/upload-server/app.js
+++ tools/upload-server/app.js
@@ -1,19 +1,17 @@
-const path = require('path');
 const express = require('express');
 const multer = require('multer');
-const { time } = require('console');
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 app.use(express.json());
 
@@ -22,29 +20,28 @@ app.post('/api/v1/measurement', (req, res) => {
   const timestamp = req.header('Timestamp');
   const {measurement_type, value} = req.body;
 
-  console.log("measurement", device_id, timestamp, measurement_type, value)
+  console.log('measurement', device_id, timestamp, measurement_type, value)
 
-  res.send({state: "success"});
+  res.send({state: 'success'});
 });
 
-app.post(
-    '/api/v1/image', upload.single('image'), async (req, res) => {
-      const device_id = req.header('Device-Id');
-      const timestamp = req.header('Timestamp');
-      const image_mime = req.header('Form-Mime');
+app.post('/api/v1/image', upload.single('image'), async (req, res) => {
+  const device_id = req.header('Device-Id');
+  const timestamp = req.header('Timestamp');
+  const image_mime = req.header('Form-Mime');
 
-      if (!req.file) {
-        return res.status(400).send('No files were uploaded.');
-      }
-      if (!device_id || !timestamp || !image_mime) {
-        return res.status(400).send('Invalid request');
-      }
+  if (!req.file) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  if (!device_id || !timestamp || !image_mime) {
+    return res.status(400).send('Invalid request');
+  }
 
-      console.log("image", device_id, timestamp, image_mime);
-      res.send({state: "success"});
+  console.log('image', device_id, timestamp, image_mime);
+  res.send({state: 'success'});
 });
 
 const port = 8080;
 app.listen(port, () => {
-    console.log("listening on", port);
+  console.log('listening on', port);
 });
